dce3db33dfd1d9cb410fd592825a405ecd540a7a
George8080 <santisjorge8000@gmail.com>
2025-04-21 01:50:21 -0400

    Creation of first text boxes for login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,67 @@ import User from './views/User';
 
 function App() {
   const [mostrarUser, actualizar_mU] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [ingresar, setLogin] = useState(false)
 
   return (
-    <div >
-      {mostrarUser ? (<User />):(
-        <button className = "button" onClick={() => actualizar_mU(true)}>Clima</button>
-      )}
-    </div>
+    <>
+      <div>
+         {/*
+        {editing ? (
+          <>
+            <input
+              value={username}
+              onChange={(e) =>
+                setUsername(e.target.value)
+              }
+            />
+            <button
+              onClick={() =>
+                setEditing(false)
+              }
+            >
+              SAVE
+            </button>
+          </>
+        ) : (
+          <>
+            <span>{username}</span>
+            <button
+              onClick={()=>
+                setEditing(true)
+              }
+            >
+              EDIT
+            </button>
+          </>
+        )}*/}
+        <span>{"Ingrese usuario: "}</span>
+        <input
+          value={username}
+          onChange={(e) =>
+            setUsername(e.target.value)
+          }
+        />
+        <br></br>
+        <span>{"Ingrese contraseña: "}</span>
+        <input
+          value={password}
+          onChange={(c) =>
+            setPassword(c.target.value)
+          }
+        />
+        <br></br>
+        <button className="button" onClick={() => setLogin(true)}>Ingresar</button>
+      </div>
+      <br></br>
+      <div>
+        {mostrarUser ? (<User />) : (
+          <button className="button" onClick={() => actualizar_mU(true)}>Clima</button>
+        )}
+      </div>
+    </>
   );
 }
 
