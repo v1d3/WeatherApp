ca34ab8b32f0c0dd57d19fe10203d27f50f0a87b
Yulissa <yulissasanhueza18@gmail.com>
2025-04-30 12:24:40 -0400

    Updated login interface design
    Modified files

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,49 @@
+import fondo from "../assets/fondo_login.png";
+import React, { useState } from 'react';
+import '../App.css';
+import User from '../views/User'; 
+import login from '../services/login.js';
+import {jwtDecode} from 'jwt-decode';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCircleUser } from '@fortawesome/free-solid-svg-icons';
+function Login(){
+    const [mostrarUser, actualizar_mU] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [userLogin, setUserLogin] = useState(false);
+    if (localStorage.getItem('UserLoged') && !userLogin) {
+      const user = JSON.parse(localStorage.getItem('UserLoged'));
+      const decoded = jwtDecode(user.data.token);
+      if(decoded.exp - Math.floor(Date.now()/1000) <= 0){
+        console.log("Cerrando secion (Tiempo expirado).");
+        localStorage.removeItem('UserLoged');
+      }
+      else{
+        setUserLogin(true);
+      }
+    } 
+    const handleLogin = async() => {
+        try{
+          const user =  await login(username, password);
+          if(user != null){
+            window.localStorage.setItem('UserLoged', JSON.stringify(user));
+            setUserLogin(true);
+            setUsername("");
+            setPassword("");
+            const decoded = jwtDecode(user.data.token);
+            console.log(decoded); // { sub: 'admin', roles: [...], iat: ..., exp: ... }
+          } 
+          else setError("Usuario o contraseña incorrectos.");
+        }catch(e){
+          console.log("ERROR: ", e);
+        }
+      }
+    
+      const logOut = async() => {
+    
+        if (localStorage.getItem('UserLoged')) {
+          console.log("Borrando usuario logeado:", JSON.parse(localStorage.getItem('UserLoged')));
           localStorage.removeItem('UserLoged');
           setUserLogin(false);
         } else {
